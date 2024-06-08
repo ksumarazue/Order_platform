4a191576e2fc4feb1488b381531dfb2a983d2249
Kamil <sumarakamil89@gmail.com>
2024-06-08 23:33:10 +0200

    Add template for users administration.

--- infra/users/user_repository.py
+++ infra/users/user_repository.py
@@ -3,11 +3,11 @@ from ..users.user import User
 
 
 
-class InvalidExpenseError(Exception):
+class InvalidUserError(Exception):
     pass
 
 
-class NotFoundExpenseError(Exception):
+class NotFoundUserError(Exception):
     pass
 
 
@@ -17,11 +17,35 @@ class UserRepository:
 
     def add_user(self, name: str, email: str, password: str, group: str):
         if not name or not email or not password or not group:
-            raise InvalidExpenseError("Invalid expense data")
-        new_user = User(user_name=name, user_email=email, user_password=password, user_group=group)
+            raise InvalidUserError("Invalid expense data")
+        print(name, email, password, group)
+        new_user = User(name=name, email=email, password=password, group=group)
         db.session.add(new_user)
         db.session.commit()
 
+    def update_user(self, id, name, email, password, group):
+        user = self.get_user_data(id)
+        if not name or not email or not password or not group:
+            raise InvalidUserError('Invalid user data')
+        if user:
+            user.name = name
+            user.email = email
+            user.password = password
+            user.group = group
+            db.session.commit()
 
-    def update_user(self):
-        pass
+    @staticmethod
+    def get_user_data(id):
+        print(f'get user data {id}')
+        user = User.query.filter_by(id=id).first()
+        if not user:
+            return NotFoundUserError(f'User ID = {id} not found')
+        else:
+            return user
+
+
+    def delete_user(self, id):
+        user = self.get_user_data(id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
