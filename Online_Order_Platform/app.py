d3b55e120fcc42c93d42d62bff48d022c8cac11c
Kamil <sumarakamil89@gmail.com>
2024-06-08 11:46:24 +0200

    Added first structure of the project. Create first user model to the database

--- Online_Order_Platform/app.py
+++ Online_Order_Platform/app.py
@@ -1,8 +1,23 @@
 from flask import Flask
+from database import db
+
 
 app = Flask(__name__)
+app.secret_key = 'secret-key'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Order_Platform.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Online_Order_Platform/models/users/user.py
@@ -0,0 +1,9 @@
+from database import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(50), unique = True, nullable = False)
+    password = db.Column(db.String(50), unique = False, nullable = False)
+    group = db.Column(db.String(50), unique = False, nullable = False)
