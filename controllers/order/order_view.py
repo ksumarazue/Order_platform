6602535c8c975d02c4b4ba34e37c2f9ff3bc4cb1
Kamil <sumarakamil89@gmail.com>
2024-06-22 13:05:09 +0200

    Add template for login and order

--- controllers/order/order_view.py
+++ controllers/order/order_view.py
@@ -13,4 +13,16 @@ def display_orders():
 
 @order_bp.route('/add', methods=['GET', 'POST'])
 def create_order():
-    pass
+    if request.method == 'POST':
+        user_name = request.form['user_name']
+        user_email = request.form['user_email']
+        user_password = request.form['user_password']
+        user_group = request.form['user_group']
+        print(user_name, user_email, user_password, user_group)
+        try:
+            user_repository.add_user(user_name, user_email, user_password, user_group)
+            flash("Successfully created expense", "success")
+            return redirect(url_for('user.display_users'))
+        except InvalidUserError as err:
+            flash(f"{err}", "danger")
+    return render_template('add_order.html')
--- infra/orders/order_manager.py
+++ infra/orders/order_manager.py
@@ -2,10 +2,23 @@ from ..orders.order import Order
 from ..database import db
 
 
+class InvalidOrderError(Exception):
+    pass
+
+
+class NotFoundOrderError(Exception):
+    pass
+
+
 class OrderRepository:
     def get_all_orders(self):
         return Order.query.all()
 
 
-    # def create_order(self):
-    #     pass
+    # def create_order(self, user_name, ):
+
+
+    # ord_id = db.Column(db.Integer, primary_key = True)
+    # ord_UsrId = db.Column(db.Integer, nullable = False)
+    # ord_UsrName = db.Column(db.String(20), nullable = False)
+    # ord_Status = db.Column(db.String(20), nullable = False)
