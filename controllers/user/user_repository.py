69ea1ee0c87a942fe0c0c3be44a099f19c108e11
Kamil <sumarakamil89@gmail.com>
2024-06-24 08:43:53 +0200

    Separate busines logid from user_view to user_repository

---
+++ controllers/user/user_repository.py
@@ -0,0 +1,46 @@
+from werkzeug.security import generate_password_hash
+
+from models.database import db
+from models.user.user import User
+
+
+class UserRepository:
+    def get_all_users(self):
+        return User.query.all()
+
+    def create_user(self, username, password, role):
+        if not username or not password or not role:
+            pass
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, password=hashed_password, role=role)
+        # new_user = User(username=username, password=hashed_password, role=role)
+        db.session.add(new_user)
+        db.session.commit()
+
+    def update_user(self, user_id, username, password, role):
+        user = self.get_user_data(user_id)
+        if not username or not password or not role:
+            pass
+        if user:
+            if username:
+                user.username = username
+            if password:
+                hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+                user.password = hashed_password
+            if role:
+                user.role = role
+            db.session.commit()
+
+    def delete_user(self, user_id):
+        user = self.get_user_data(user_id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+
+    @staticmethod
+    def get_user_data(user_id):
+        user = User.query.get(user_id)
+        if not user:
+            pass
+        else:
+            return user
--- controllers/user/user_view.py
+++ controllers/user/user_view.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template, redirect, url_for, request, session, flash
-from werkzeug.security import generate_password_hash, check_password_hash
-from models.database import db
+from werkzeug.security import check_password_hash
+
+from controllers.user.user_repository import UserRepository
 from models.user.user import User
 
 user_blueprint = Blueprint('user', __name__)
+
+user_repository = UserRepository()
 
 @user_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
@@ -42,29 +45,15 @@ def admin():
         user_id = request.form.get('user_id')
 
         if action == 'create':
-            hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
-            new_user = User(username=username, password=hashed_password, role=role)
-            db.session.add(new_user)
-            db.session.commit()
+            user_repository.create_user(username, password, role)
             flash(f'User {username} created successfully.', 'success')
         elif action == 'update':
-            user_to_update = User.query.get(user_id)
-            if username:
-                user_to_update.username = username
-            if password:
-                user_to_update.password = generate_password_hash(password, method='pbkdf2:sha256')
-            if role:
-                user_to_update.role = role
-            db.session.commit()
-            flash(f'User {user_to_update.username} updated successfully.', 'success')
+            user_repository.update_user(user_id, username, password, role)
+            flash(f'User updated successfully.', 'success')
         elif action == 'delete':
-            user_to_delete = User.query.get(user_id)
-            db.session.delete(user_to_delete)
-            db.session.commit()
-            flash(f'User {user_to_delete.username} deleted successfully.', 'success')
-
-    users = User.query.all()
-    return render_template('admin.html', users=users, user=user)
+            user_repository.delete_user(user_id)
+            flash(f'User deleted successfully.', 'success')
+    return render_template('admin.html', users=user_repository.get_all_users(), user=user)
 
 @user_blueprint.route('/home')
 def home():
