3dd5cbfefa202846557abad53385809078ab3dbd
Kamil <sumarakamil89@gmail.com>
2024-06-26 21:26:15 +0200

    Remove unused files

--- infra/users/user.py
+++
@@ -1,9 +0,0 @@
-from infra.database import db
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(20), unique = True, nullable = False)
-    email = db.Column(db.String(50), unique = True, nullable = False)
-    password = db.Column(db.String(50), unique = False, nullable = False)
-    group = db.Column(db.String(50), unique = False, nullable = False)
